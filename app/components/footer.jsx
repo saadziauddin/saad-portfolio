ed10cb8338508509252b79fff38153b08f20b46a
Saad <saad.ziauddin@pronet-tech.net>
2024-09-05 12:54:53 +0500

    New Updates

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/saad-dev07"
+              href="https://github.com/saad-devs07"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,7 +25,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/saad-dev07/fork"
+              href="https://github.com/saad-devs07/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
