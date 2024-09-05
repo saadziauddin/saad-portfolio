52cadd4b3791bef2da4c2715b0ebd1dfb5378c16
Saad <saad.ziauddin@pronet-tech.net>
2024-09-05 12:38:11 +0500

    New Update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,8 @@ export const personalData = {
   name: "Saad",
   profile: '/profile1.png',
   designation: "Software Engineer",
-  description: "My name is Saad. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on MERN Stack and DevOps. I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests. I'm driven by a commitment to excellence and continuous improvement, and I thrive in collaborative environments where teamwork and innovation are valued. I am eager to leverage my skills and passion for technology to contribute to a forward-thinking company.",
+  description: `I'm Saad, a passionate and professional programmer with a self-learning attitude. I specialize in the MERN stack and DevOps, with a strong focus on JavaScript. I love exploring new technologies and solving complex problems, constantly striving to make the web more accessible.
+  A quick learner, I enjoy working across all web development stacks and adapting to new challenges. My drive for continuous improvement pushes me to stay ahead of industry trends, always looking for ways to enhance the quality and efficiency of my work. My commitment to quality and excellence ensures that every project I work on is a success.`,
   email: 'saadziauddin6@gmail.com',
   phone: '+923052586061',
   address: 'Defence View Society, Phase 1, Karachi, Pakistan.',
@@ -11,7 +12,5 @@ export const personalData = {
   twitter: 'https://twitter.com/@saad-dev07',
   dev: 'https://dev.to/saad-dev07',
   whatsapp: "https://wa.me/+923052586061?text=Hi%20Saad%2C%20I%20have%20viewed%20your%20portfolio%2C%20tell%20me%20what%20services%20are%20you%20offering.",
-  resume: "https://drive.google.com/file/d/1BiCReS5xKTc-wKzF050RY9_-FeadO5tL/view?usp=drive_link"
+  resume: "https://drive.google.com/file/d/1aM2DGrCmIe0pKSGlYSYhrrnAPqt4mN2V/view?usp=drive_link"
 }
-
-// I am Saad, an enthusiastic and innovative Software Engineer and Full Stack Developer with a deep passion for technology and problem-solving. Currently, I am pursuing an Advanced Diploma in Software Engineering (ADSE) at Aptech and a Bachelor's degree in Information Technology (BSIT) from a well-renowned university. My academic and self-taught experiences have equipped me with a robust foundation and the agility to adapt to new challenges and technologies swiftly. I am adept at designing and developing responsive websites and sophisticated software solutions for small to medium-sized businesses. My professional journey has honed my ability to quickly grasp new concepts, think creatively, and develop efficient, innovative solutions to complex problems. I specialize in building advanced Interactive Voice Response (IVR) systems that streamline customer service operations by automating interactions and providing robust self-service options. My expertise with Avaya Orchestration Designer enables me to design efficient and user-friendly IVR systems tailored to the unique needs of call centers and contact centers. I am driven by a commitment to excellence and continuous improvement, and I thrive in collaborative environments where teamwork and innovation are valued. I am eager to leverage my skills and passion for technology to contribute to a forward-thinking company. If you are looking for a dedicated developer, intern, or trainee who can deliver high-quality solutions and drive technological advancement, I would welcome the opportunity to discuss how I can be an asset to your team.
