366afc4498d1e3f6e7c14003b41c666382be7fe1
Saad <saad.ziauddin@pronet-tech.net>
2024-09-05 15:20:05 +0500

    New Update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,8 +2,10 @@ export const personalData = {
   name: "Saad",
   profile: '/profile1.png',
   designation: "Software Engineer",
-  description: `I'm Saad, a passionate and professional programmer with a self-learning attitude. I specialize in the MERN stack and DevOps, with a strong focus on JavaScript. I love exploring new technologies and solving complex problems, constantly striving to make the web more accessible.\n
-  A quick learner, I enjoy working across all web development stacks and adapting to new challenges. My drive for continuous improvement pushes me to stay ahead of industry trends, always looking for ways to enhance the quality and efficiency of my work. My commitment to quality and excellence ensures that every project I work on is a success.`,
+  description: 
+      `I'm Saad, a passionate and professional programmer with a self-learning attitude. I specialize in the MERN stack and DevOps, with a strong focus on JavaScript. I love exploring new technologies and solving complex problems, constantly striving to make the web more accessible.
+      /n
+      A quick learner, I enjoy working across all web development stacks and adapting to new challenges. My drive for continuous improvement pushes me to stay ahead of industry trends, always looking for ways to enhance the quality and efficiency of my work. My commitment to quality and excellence ensures that every project I work on is a success.`,
   email: 'saad.ziauddin07@gmail.com',
   phone: '+923052586061',
   address: 'Defence View Society, Phase 1, Karachi, Pakistan.',
