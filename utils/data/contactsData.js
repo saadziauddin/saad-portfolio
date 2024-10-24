8a28d5e92834ad7c9a710f3e6760615033d4212b
Saad <saad.ziauddin@pronet-tech.net>
2024-10-24 15:47:14 +0500

    new commit

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,9 +1,9 @@
 export const contactsData = {
-    email: 'saadziauddin6@gmail.com',
+    email: 'saad.ziauddin07@gmail.com',
     phone: '+923052586061',
     address: 'Defence View Society, Phase 1, Karachi.',
     linkedIn: 'https://www.linkedin.com/in/saad-ziauddin',
-    github: 'https://github.com/saadziaudin',
+    github: 'https://github.com/saad-devs07',
     twitter: 'https://twitter.com/@itx_saadii173',
     dev: 'https://dev.to/saadziauddin',
     dev: "saadziauddin"
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,9 +6,9 @@
 export const projectsData = [
     {
         id: 1,
-        name: "Lawyer's Website",
-        description: "Me and my team built a A Lawyer's Website. A Complete dynamic website with admin panel, lawyers panel & client panel. Created using HTML5, CSS3, JavaScript, Tailwind CSS, PHP, MySQL. I added role-based authentication for clients, lawyers and admin. Lawyers booking system and approve request and reject request from lawayer and many more features I added in this.",
-        tools: ['HTML5', 'CSS3', 'JavaScript', 'Tailwind CSS', 'PHP', 'MySQL'],
+        name: 'Nayab Fashion (E-commerce)',
+        description: 'I have designed and developed a full-stack web application for Nayab Fashion (Clothing Store). I created this website using Vite.js, React.js, Express.js, Node.js, JavaScript, Tailwind CSS and MongoDB. I added role-based authentication for Admins, Sub-Admins, Customers, including userfriendly dashboard, online payment integration. I added currency feature for multiple countries and many more features I added in this. Currently working on it will be live soon...',
+        tools: ['Vite.js', 'React.js', 'Express.js', 'Node.js', 'Tailwind CSS', 'MongoDB', 'JavaScript'],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
@@ -16,9 +16,9 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'Blackmount Films Website',
-        description: 'I have designed and developed a full-stack web app for Blackmount Films. I created this website using HTML5, CSS3, JavaScript, Tailwind CSS and SQL SERVER. I added role-based authentication for users and admin, subscription plans, event booking system and payment integration and many more features I added in this.',
-        tools: ['HTML5', 'CSS3', "Tailwind CSS", "SQL SERVER", "JavaScript"],
+        name: 'Blackmount Films (Event Mangement Company)',
+        description: 'I have designed and developed a full-stack web app for Blackmount Films (Corporate, Food, Wedding, Indoor and Outdoor (Photography, Videography & Drone Shoots)). I created this website using Vite.js, React.js, Express.js, Node.js CSS3, JavaScript, Tailwind CSS and MongoDB. I added role-based authentication for users and admin, subscription plans, event booking system and payment integration, added currency feature for multiple countries and many more features I added in this.',
+        tools: ['Vite.js', 'React.js', 'Express.js', 'Node.js', 'Tailwind CSS', 'MongoDB', 'JavaScript'],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
@@ -26,11 +26,11 @@ export const projectsData = [
     },
     {
         id: 3,
-        name: 'Quick Snacks Website',
-        description: 'Me and my team built Quick Snacks Website. We used Asp.net core, C#, Nudget Packages, JavaScript, and SQL Server to develop the Website. We utilized HTML5, CSS3, Bootstrap for the UI. We added role-based authentication, add to cart and check out feature, order booking system, and payment integration system.',
-        tools: ['HTML5', 'CSS3', 'Bootstrap', 'Asp.net core', 'Nudget Packages', 'C#', 'JavaScript', 'SQL Server'],
+        name: "Lawyer's Website",
+        description: "Me and my team built a A Lawyer's Website. A Complete dynamic website with admin panel, lawyers panel & client panel. Created using HTML5, CSS3, JavaScript, Tailwind CSS, PHP, MySQL. I added role-based authentication for clients, lawyers and admin. Lawyers booking system and approve request and reject request from lawayer and many more features I added in this.",
+        tools: ['HTML5', 'CSS3', 'JavaScript', 'Tailwind CSS', 'PHP', 'MySQL'],
+        role: 'Full Stack Developer',
         code: '',
-        role: 'Backend Developer',
         demo: '',
         image: '',
     },
@@ -43,5 +43,15 @@ export const projectsData = [
         demo: '',
         image: '',
         role: 'Full Stack Developer',
-    }
+    },
+    {
+        id: 5,
+        name: 'Quick Snacks Website',
+        description: 'Me and my team built Quick Snacks Website. We used Asp.net core, C#, Nudget Packages, JavaScript, and SQL Server to develop the Website. We utilized HTML5, CSS3, Bootstrap for the UI. We added role-based authentication, add to cart and check out feature, order booking system, and payment integration system.',
+        tools: ['HTML5', 'CSS3', 'Bootstrap', 'Asp.net core', 'Nudget Packages', 'C#', 'JavaScript', 'SQL Server'],
+        code: '',
+        role: 'Backend Developer',
+        demo: '',
+        image: '',
+    },
 ];
