eca4205c72682d94be824ac0f8405c8349d95f66
Saad <saad.ziauddin@pronet-tech.net>
2024-10-24 16:21:28 +0500

    new commit

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,9 +2,9 @@ export const contactsData = {
     email: 'saad.ziauddin07@gmail.com',
     phone: '+923052586061',
     address: 'Defence View Society, Phase 1, Karachi.',
-    linkedIn: 'https://www.linkedin.com/in/saad-ziauddin',
+    linkedIn: 'https://www.linkedin.com/',
     github: 'https://github.com/saad-devs07',
-    twitter: 'https://twitter.com/@itx_saadii173',
+    twitter: 'https://twitter.com/',
     dev: 'https://dev.to/saadziauddin',
     dev: "saadziauddin"
 }
